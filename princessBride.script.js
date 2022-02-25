cbd822b0cf25ebed2b0b6d206efbb2b8e97406c8
Job85 <julianjernigan@ymail.com>
2022-02-25 01:27:06 -0500

    changed back to first save

--- princessBride.script.js
+++ princessBride.script.js
@@ -1,7 +1,7 @@
 let valueOfPrompt = prompt('Would you rather deal with Biff or Griff? (B/G)'); {
     if (valueOfPrompt === 'B' || valueOfPrompt === 'b') {
     prompt('Hmm, interesting. Biff is angry and has a cane. Do you stand and fight or run away like a coward? (S/R)')
-    } else if (valueOfPrompt === 'G'|| valueOfPrompt === 'g') {
+} else if (valueOfPrompt === 'G'|| valueOfPrompt === 'g') {
         prompt('Griff is asking you if you are in or out. What do you say? (I/O)')
     }
 }
