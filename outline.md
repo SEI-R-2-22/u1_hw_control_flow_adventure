3555c96d2cd0256c74c2a83467ff811e93e5eb6c
timmellis <tim.m.ellis@gmail.com>
2022-02-25 00:11:48 -0500

    first commit, about halfway to 2/3 through

---
+++ outline.md
@@ -0,0 +1,83 @@
+# 3 Little Pigs
+
+### THE RULES:
+> - ANY path must have **3** options.
+> - Min 7 final endings.
+> - At least 1 question there must be **more than 2** possible responses.
+> - Must contain at least one "switch" statement.
+> - Must make use of both strings and numbers inputs.
+> - Must have option to replay.
+
+## OUTLINE
+
+- Name (prompt)
+- "Hello [name]. What a handsome wolf you are. You are walking along a road when your tummy starts rumbling. Up ahead you see 3 houses. Which do you visit?
+  - Straw
+    - Cute house. You can see a delicious piggy inside through the little window. What do you do?
+      - [H]uff, puff and blow
+        - How many times? (prompt #)
+          - (>=1) : I mean, this was a piece of cake. Or, pork. You gobble up the little piggy. Delish. 
+          - (>3) : Overkill much? You blow the house down, but you also hyperventilate. The piggy escapes. 
+      - [K]nock politely on the door.
+        - Interesting ploy. The piggy calls "who is it?" What is your reply?
+          - [F]ib. "It's Red Riging Hood! I brought snacks from Grandmother's house!"
+            - "That's not Red's voice!" cries the piggy. It scarpers out the back and escapes. Nice try.
+          - [T]ell the Truth: "It' the big bad wolf, and I would like to eat you, please!"
+            - Predictably, the piggy is not excited to see you. "Aaahhh!" It yells, as it runs out the back, and escapes. No dinner for you. 
+          - [L]and-Shark.
+            - "What?" the piggy says. It opens the door. You gobble it up. I cannot believe that worked. 
+      - [W]ait until dark. 
+        - You ring up you old pal Rumpelstiltskin, who works through the night spinning the straw into gold. By morning, you've completely forgotten about the piggy and instead use your new-found treasure to feast at the local BBQ joint. Win Win.
+
+
+
+  - Sticks
+      - Sturdy, but not quite up to Lincoln's log cabin standards. You can see a delicious piggy inside through the thin glass windows. What do you do?
+      - [H]uff, puff and blow
+        - How many times? (prompt #)
+          - (<2) : You see it quiver, but it doesn't quite fall down. Meanwhile, all your huffing and puffing has alerted the piggy to your presense, and it escapes out the back. Drat.
+          - (2<=n<=3) : Success! You easily extract the piggy from the kindling that was once its house, and gobble it up. Delish.
+          - (>3) : Overkill, hyperventilate...
+      - [K]nock politely on the door
+        - Interesting ploy. The piggy angrily calls "who's there'?" What is your reply?
+          - [F]ib. "Um, I'm just another little piggy, just like you!" you call. 
+            - "Which one?" grumbles the piggy. Hmm.
+              - [1]st little piggy, who went to market.
+              - [2]nd little piggy, who stayed home.
+              - [3]rd little piggy, who had roast beef.
+                - (if response === 3) "Did you save me any roast beef?" [Y/N]
+                  - [Y] The piggy greedily flings open the door. You waste no time gobbling it up. Yummy, but now you really want roast beef.
+                  - [N] "Well then get off my lawn!" A frying pan is thrown from the window with surprising accuracy. You run away, tail literally between your legs. Better luck next time. 
+              - [4]th little piggy, who had none.
+              - [5]th little piggy, who went 'wee wee wee wee' all the way home.
+              - (All others) "You liar! I'M that little piggy! You're the wolf, aren't you?" The piggy runs out the back an escapes. You're a terrible liar. 
+          - [T]ell the Truth: "It' the big bad wolf, and I would like to eat you, please!"
+            - The door is flung open, but you find yourself staring down the barrel of a rifle. What is this, Animal Farm?! "You have 5 seconds..." the piggy begins, but you don't wait around to find out what for. This did not go how you planned. Apparently no dinner for you. 
+          - [L]and-Shark.
+            - "That's an old reference," says the piggy. It escapes out the back before you have time to say "Candy gram." Bummer.       
+      - [W]ait until dark
+        - You hide until the sun goes down, when the piggy lights a merry fire in the fireplace before bed, without realizing that its house is literally made of kindling. Long story short, there's nothing better than a good roast pig. 
+        
+  - Brick
+      - House?! This place is a goddamned castle! Still, you can see a delicious piggy inside through arched window. What do you do?
+      - [H]uff, puff and blow
+        - (<=3) Well, obviously nothing happens. And all the huffing and puffing has alerted the piggy. And his archers. Crap.
+        - (>3) Hyperventilate AND do nothing. The piggy laughs at you from the gatehouse while the guards come to collect your sorry self. I'm sure you'll have a lovely future as a throw rug. 
+      - [K]nock politely on the door.
+        - Interesting ploy. "Who is it?" the piggy calls calmly. What is your reply?
+          - [F]ib. "Castle inspector!" You call back. 
+            - "Ha," the piggy laughs, not remotely convinced. "The castle inspector was alerady here LAST week. Good thing, too, otherwise I would never have gotten my catapault back into working condition! Wanna see?" That doesn't sound good...
+          - [T]ell the Truth: "It's the big bad wolf, and I would like to eat you, please!"
+            - "I appreciate your candor." Says the piggy as archers line up along the walls. "Tell you what: I have no interest in being eaten, and you have no interest in being a throw rug. Shall we negotiate a truce?" You weigh your options.  
+          - [L]and-Shark.
+            - "What?"
+              - [P]lumber.
+                - "I didn't order a plummer."
+              - [F]lowers. ...Um, candy gram.
+                - "No seriously, who is it?"
+                  - [J]ehovah's Witness
+                  - []
+
+
+      - [W]ait until dark. 
+        
--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,196 @@
+// Helper functions and other elements to simplify things that are repeated
+const approachTheHouseOptions = `
+  [H]uff, puff, and blow the house down.
+  [K]nock politely on the door.
+  [C]all for help.
+  `;
 
+const phoneAFriend = `
+  [F]ire Lord Ozai.
+  [R]umplestiltskin.
+  [G]rond.
+`;
+
+const grondQuote = `The drums rolled louder. Fires leaped up. Great engines crawled across the field; a huge ram, great as a forest-tree a hundred feet in length, swinging on mighty chains. Long had it been forging in the dark smithies of Mordor, and its hideous head, founded of black steel, was shaped in the likeness of a ravening wolf; on it spells of ruin lay. Grond they named it, in memory of the Hammer of the Underworld of old. Great beasts drew it, orcs surrounded it, and behind walked mountain-trolls to wield it.
+
+...The drums rolled and rattled. With a vast rush Grond was hurled forward by huge hands. It reached the Gate. It swung. A deep boom rumbled through the City like thunder running in the clouds.`;
+
+// ********* STORY BEGINS HERE *************** //
+
+const user = prompt("Hello! what is your name?");
+
+alert(`Hello ${user}. What a handsome wolf you are. And my, what big teeth you have.`);
+
+alert(`(Nope, sorry, wrong story. Ahem.)`);
+
+const whichHouse = prompt(`You are walking along a road when your tummy starts rumbling. Up ahead you see 3 houses: One made of straw, one made of sticks, and one made of brick. Which do you visit? 
+
+(type one of the following):
+
+[S]traw 
+S[t]icks 
+[B]rick`);
+
+//// First choice: which house to go into:
+
+// HOUSE OF STRAW
+if (whichHouse.toLowerCase() === "straw" || whichHouse.toLowerCase() === "s") {
+  const houseAction = prompt(`You approach the straw house. Cute. You can see a delicious little pig inside through the tiny window. Your mouth begins to water. What do you do?
+  ${approachTheHouseOptions}
+  `); 
+
+  // STRAW > HUFF AND PUFF
+  if (houseAction.toLowerCase() === "h") {
+    
+    const huffPuffCount = prompt(`Classic. How many times do you huff and puff before trying to blow the house down?`);
+    
+    // Check how many times they huf and puff, parseInt
+    if (parseInt(huffPuffCount) < 3) {
+      alert(`I mean, yeah, it's a straw house, so this is a piece of cake. Or -- as it happens -- pork. 
+      
+      You gobble up the little pig. Delish. Well done.`); ////// END CONDITION !!
+    }
+    else if (parseInt(huffPuffCount) >= 3) {
+      alert(`Overkill much? You blow the house down, but your lungs aren't what they used to be, so you also hyperventilate. The piggy escapes while you catch your breath. 
+      
+      Better luck next time.`); ////// END CONDITION !!
+    }
+  
+    // STRAW > KNOCK POLITELY
+  } else if (houseAction.toLowerCase() === "k") {
+  
+    const knockResponse = prompt();
+  
+    // STRAW > CALL FOR HELP
+  } else if (houseAction.toLowerCase() === "c") {   
+    const whoYouGonnaCall = prompt(`Teamwork makes the dream work. Who you gonna call?
+    
+    ${phoneAFriend}`);
+
+    // STRAW > CALL > RUMPLESTILTSKIN
+    if (whoYouGonnaCall.toLowerCase() == "r") {
+      alert(`You ring up you old pal Rumpelstiltskin, who works through the night spinning the straw into gold. By morning, you've completely forgotten about the little pig (who ran off hours ago) and instead use your new-found treasure to completely buy out the local BBQ joint. 
+      
+      Win Win! Congrats!`); //// WIN CONDITION !!!
+    }
+    // STRAW > CALL > NOT HOME
+    else {
+      alert(`It rings and rings, but no one answers. Maybe you got the wrong number? Maybe they're screening their calls? Either way, you've wasted too much time. The little pig spotted you and vamoosed. 
+      
+      Better luck next time.`); // END CONDITION !!
+    }
+
+
+  } else alert("sorry, that wasn't an option.");
+
+} // end elseif strawHouse
+
+// HOUSE OF STICKS
+else if (whichHouse.toLowerCase() === "sticks" || whichHouse.toLowerCase() === "t") {
+  const houseAction = prompt(`You approach the house of sticks. It is sturdy, but not quite up to "Lincoln's log cabin" standards. Through the stick-framed windows you see a delicious little pig. Your mouth begins to water. What do you do?
+  ${approachTheHouseOptions}
+  `);
+
+  // STICKS > HUFF AND PUFF
+  if (houseAction.toLowerCase() === "h") {      // Huff puff and blow
+    
+    const huffPuffCount = prompt(`Classic. How many times do you huff and puff before trying to blow the house down?`);
+    
+    // check how many times they huff and puff
+    if (parseInt(huffPuffCount) == 1) {
+      alert(`You see the house of sticks tremble, but it doesn't quite fall down. Turns out this place was a little sturdier than you thought. 
+      
+      Meanwhile, all that huffing and puffing has alerted the little pig to your presense, and it escapes out the back door. Drat. 
+      
+      Better luck next time.`); ////// END CONDITION !!
+    }
+    else if (parseInt(huffPuffCount) == 2) {
+      alert(`Success! With a tremendous breath, you are able to topple the house of sticks. 
+      
+      You easily extract the piggy from the kindling that was once its house, and gobble it up. Yum!`);  ////// END CONDITION !!
+    }
+    else if (parseInt(huffPuffCount) >= 3) {
+      alert(`Overkill much? You blow the house down, but your lungs aren't what they used to be, so you also hyperventilate. The piggy escapes while you catch your breath. 
+      
+      Better luck next time.`); ////// END CONDITION !!
+    }
+  } 
+  
+  // STICKS > KNOCK POLITELY
+  else if (houseAction.toLowerCase() === "k") {   // Knock politely
+
+  } 
+  // STICKS > call for help
+  else if (houseAction.toLowerCase() === "c") {   // call for help
+
+    if (whoYouGonnaCall.toLowerCase() == "f") {
+      alert(`You ring the great Fire Lord Ozai, who loves nothing more than to set this little house ablaze.  
+      
+      Successful pig roast! Yum!`); //// WIN CONDITION !!!
+    }
+    // STRAW > CALL > NOT HOME
+    else {
+      alert(`It rings and rings, but no one answers. Maybe you got the wrong number? Maybe they're screening their calls? Either way, you've wasted too much time. The little pig spotted you and vamoosed. 
+      
+      Better luck next time.`); // END CONDITION !!
+    }
+
+
+  } else alert("sorry, that wasn't an option.");
+
+
+} // end elseif stickHouse
+
+//HOUSE OF BRICK
+else if (whichHouse.toLowerCase() === "brick" || whichHouse.toLowerCase() === "b") {
+  const houseAction = prompt(`House?! This place is a goddamned castle! The stories have not done this place justice. Still, you can see a delicious little pig inside through greate vaulted windows. So. What do you do?
+  ${approachTheHouseOptions}
+  `);
+
+  // BRICK > HUFF AND PUFF
+  if (houseAction.toLowerCase() === "h") {
+
+    const huffPuffCount = prompt(`Classic, if unlikely. Okay then... How many times do you huff and puff before trying to blow the house down?`);
+    
+    //    Check how many times they huff and puff
+    if (parseInt(huffPuffCount) < 3) {
+      alert(`Well obviously you don't blow down the freakin' castle. Plus, all this huffing and puffing has alerted Baron von Pig. And... their archers. Crap.
+      
+      You high-tail it out of there. Better luck next time.`); ////// END CONDITION !!
+    }
+    else if (parseInt(huffPuffCount) >= 3) {
+      alert(`What were you thinking? Obviously you don't blow down the freakin' castle, AND you also hyperventilate. The little pig laughs at you from the gatehouse, while their guards come to collect your sorry self. I'm sure you'll have a lovely future as a throw rug or something.
+      
+      Let's pretend this didn't happen.`); ////// END CONDITION !!
+    }
+    
+  } 
+  
+  // BRICK > KNOCK POLITELY
+  else if (houseAction.toLowerCase() === "k") {   
+
+  } 
+
+  // BRICK > CALL FOR HELP
+  else if (houseAction.toLowerCase() === "c") {   
+
+    if (whoYouGonnaCall.toLowerCase() == "g") {
+      alert(`As night falls... 
+      
+      "${grondQuote}"
+      
+      Snooty little pig didn't see THAT one coming!
+      
+      You feast on all the little pigs in the castle. (You do have to share with the orcs, though.)`); //// WIN CONDITION !!!
+    }
+    // STRAW > CALL > NOT HOME
+    else {
+      alert(`It rings and rings, but no one answers. Maybe you got the wrong number? Maybe they're screening their calls? Either way, you've wasted too much time. The little pig spotted you and vamoosed. 
+      
+      Better luck next time.`); // END CONDITION !!
+    }
+
+
+  } else alert("sorry, that wasn't an option.");
+
+} // end elseif brickHouse
