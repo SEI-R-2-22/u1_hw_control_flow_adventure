8c9ae5b3cbb067cac1a809ff110fc28bcb393323
Job85 <julianjernigan@ymail.com>
2022-02-26 02:32:44 -0500

    added second flow for Dean

--- adventureFlow.js
+++ adventureFlow.js
@@ -6,25 +6,34 @@ if(readyForAdventure == true);{
 if (characterSelect == 'Dean') {
     let deanVentureBegin = prompt('You chose some brain over brawn. Will you get far? Y/N')
     if (deanVentureBegin == 'Y') {
-        console.log('Y')
-    }    else if (deanVentureBegin == 'N') {
-            console.log('N')
+        let deanFirstChoice = prompt('The world is your oyster Dean. Pick your mode of transport: Boat, Plane, Car')
+        if (deanFirstChoice == 'Boat') {
+            console.log('X2')
+        }
+        else if (deanFirstChoice == 'Plane') {
+            console.log('X1')
+        }
+        else if (deanFirstChoice == 'Car') {
+            console.log('Adrianne')
+        }
+    }else if (deanVentureBegin == 'N') {
+            alert('Some lame adventure that was, Dean.')
         }
 }
 else if (characterSelect == 'Hank') {
     let hankVenutreBegin = prompt('A daring young man. But will you get far? Y/N')
     if (hankVenutreBegin == 'Y') {
         console.log('Y')
-    }    else if (hankVenutreBegin == 'N') {
-            console.log('N')
+    }else if (hankVenutreBegin == 'N') {
+            alert('You tell H.E.L.P.eR. to make you a sandwhich. But fall asleep before he\'s done')
         }
 }
 else if (characterSelect == 'Brock'){
     let brockSamsonBegin = prompt('A one-half Swedish, one-quarter Polish, one-quarter Winnebago "murder machine". But will you get far? Y/N')
     if (brockSamsonBegin == 'Y') {
         console.log('Y')
-    }    else if (brockSamsonBegin == 'N') {
-            console.log('N')
+    }else if (brockSamsonBegin == 'N') {
+            alert('Has the mighty Brock Sampson gone soft?')
         }
 }
 }
