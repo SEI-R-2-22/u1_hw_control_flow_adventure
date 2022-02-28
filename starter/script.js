9623f902b6c902931fa26c5edd351e4a839b2939
Shane Brooks <shanedbrooks06@gmail.com>
2022-02-27 20:05:50 -0500

    test

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,9 @@
+let valueOfPrompt = prompt("Great, just your luck. You are being robbed by two masked individuals. One appears to have a knife, the other one is behind you, no idea if they are armed. What is your plan? GIVE up items, PLEAD with robbers, attempt DISARM.(Give, Plead, Disarm)");
+console.log(valueOfPrompt)
 
+switch(valueOfPrompt) {
+case 'Give':
+  let calm = prompt("You're shaking.. but so is the robber, Do you stay CALM or continue to act FIGGITY? (Calm/Figgity)");  
+    
+
+}
