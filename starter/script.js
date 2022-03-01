ff292ce6fdba4dc70441bfe8e7b84d24d13c2c9c
Michelle Flores <lmfg2456@gmail.com>
2022-03-01 13:04:52 -0800

    control_flow_homework

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,63 @@
+let catDog = prompt('Adopt cat or dog')
+alert(`I want to adopt a ${catDog} .`)
+
+if catDog = 
+{
+    const story = 0;
+    const form = document.getElementById('theAdoption'); 
+    const submit = documen.getElementById('continueButton');
+    const reset = document.getElementById('resetButton')
+    const answer = '';
+     console.log (form);
+    }
+     const storyTime = 1;
+     {
+        "start":{
+            "question": "Do you enjoy endless attention or prefer a calm companion?",
+            "answers":{
+                "a": "Calm",
+                "b": "Lots of attention",
+                "c": "Undecided",
+            }  
+        },
+        // 1 option
+        "startTwo":{
+            "question": "Would a kitten be puuuurrfect for you?"
+            "answers": {
+                "a": "Yes but what if it scratches me ",
+                "b": "No, they're grumpy",
+                "c": "Maybe tell me a bit about dogs",
+            }
+        }
+        
+       
+         // continue Link
+         submit.addEventListener('mouseup'), function (){
+            answer = form.querySelectorAll('input[type=radio]:checked')[0].value; 
+            if(answer){
+                story++;
+                populateForm(story + '_' + answer);
+                console.log("Time to adopt!")
+            }
+        
+        }
+        
+       
+         // Reset button
+         function resetForm(){
+            document.getElementById
+        }   
+        
+        
+        
+        
+        
+        
+        
+        
+
+
+
+
+
 
